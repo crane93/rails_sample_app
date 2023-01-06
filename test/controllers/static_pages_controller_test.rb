e9cb091816ba2936d2bddd6424e2c8e744fd1380
crane93 <youl9094@gmail.com>
2023-01-06 09:26:44 +0000

    Add static page for about

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,17 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+    get static_pages_home_url #get리퀘스트를 home액션에 대해 실행
+    assert_response :success  #그러면 결과가 success(http스테이터스코드가 200)가 될것이다
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 end
